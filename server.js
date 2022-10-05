f2cb5d3f88aefbad7e4247755a6c2c96a33f4a27
Michelle Bell <mbell21@myseneca.ca>
2022-10-05 17:00:07 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+/*********************************************************************************
+* BTI325 – Test 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Michelle Bell Student ID: 137551214 Date: Sunday, September 19, 2022
+*
+* Online (Cyclic) URL:
+* //https://misty-scrubs-bass.cyclic.app
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
@@ -12,4 +29,4 @@ app.get("/test1", (req, res) => {
 });
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT, onHttpStart);
